85d8205e77ca889ac5920a4d2b0159aa40c48150
Omar Abdelmohsen <165558568+omar-khalifa21@users.noreply.github.com>
2026-05-18 18:44:25 +0300

    Add: two-integer-sum - submission-10

---
+++ Data Structures & Algorithms/two-integer-sum/submission-10.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+       numap = {}
+       for i in range(len(nums)):
+       
+        if target-nums[i] in numap :
+         return [numap[target-nums[i]],i]
+        numap[nums[i]]=i 
+        
+      
+      
